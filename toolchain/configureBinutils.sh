64d9a7ebcd06801ac8b00b4efeab8345169b4ab5
David Price <DPriceDev@Gmail.com>
2022-01-09 20:53:40 +0000

    Cleaned up cmake and configure scripts

--- toolchain/configureBinutils.sh
+++ toolchain/configureBinutils.sh
@@ -5,4 +5,11 @@ cd $CONFIG_DIR
 patch -p1 < $PATCH_DIR/binutils.patch
 popd
 
-sh $CONFIG_DIR/configure CFLAGS='-g0 -O2' CXXFLAGS='-g0 -O2' --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT --disable-nls --disable-werror
+sh $CONFIG_DIR/configure \
+  CFLAGS='-g0 -O2' \
+  CXXFLAGS='-g0 -O2' \
+  --target=$TARGET \
+  --prefix=$PREFIX \
+  --with-sysroot=$SYSROOT \
+  --disable-nls \
+  --disable-werror
--- toolchain/configureGcc.sh
+++ toolchain/configureGcc.sh
@@ -5,4 +5,12 @@ cd $CONFIG_DIR
 patch -p1 < $PATCH_DIR/gcc.patch
 popd
 
-sh $CONFIG_DIR/configure CFLAGS='-g0 -O2' CXXFLAGS='-g0 -O2' --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT --with-newlib --disable-nls --enable-languages=c,c++
+sh $CONFIG_DIR/configure \
+  CFLAGS='-g0 -O2' \
+  CXXFLAGS='-g0 -O2' \
+  --target=$TARGET \
+  --prefix=$PREFIX \
+  --with-sysroot=$SYSROOT \
+  --with-newlib \
+  --disable-nls \
+  --enable-languages=c,c++
