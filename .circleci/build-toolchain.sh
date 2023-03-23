a562fc13de70484532ec4323a24646e424e0a963
David Price <DPriceDev@Gmail.com>
2023-03-23 17:58:53 +0000

    moved toolchain step to script and added skip

---
+++ .circleci/build-toolchain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d "/path/to/dir" ]
+then
+  exit 0
+fi
+
+sudo apt-get update
+sudo apt-get install texinfo flex bison nasm graphviz doxygen ccache libmpfr-dev libmpc-dev doxygen --no-install-recommends -y
+
+wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.tar.gz
+tar -zxvf cmake-3.26.0-linux-x86_64.tar.gz
+cd cmake-3.26.0-linux-x86_64 || exit 1
+sudo cp -r bin /usr/
+sudo cp -r doc /usr/share/
+sudo cp -r man /usr/share/
+sudo cp -r share /usr/
+cmake --version
+
+mkdir -p cmake-build-release && cmake -DCMAKE_BUILD_TYPE=Release -B cmake-build-release
+cmake --build cmake-build-release --target gcc-12.2.0
