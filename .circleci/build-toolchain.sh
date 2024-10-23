d8399cde60ed26d7a56b7367760f6cbb869d3c51
David Price <DPriceDev@gmail.com>
2024-10-23 21:45:09 +0100

    Update cmake version in download link

--- .circleci/build-toolchain.sh
+++ .circleci/build-toolchain.sh
@@ -8,7 +8,7 @@ fi
 sudo apt-get update
 sudo apt-get install texinfo flex bison nasm graphviz doxygen ccache libmpfr-dev libmpc-dev doxygen --no-install-recommends -y
 
-wget https://github.com/Kitware/CMake/releases/download/v3.26.0/cmake-3.26.0-linux-x86_64.tar.gz
+wget https://github.com/Kitware/CMake/releases/download/v3.30.5/cmake-3.30.5-linux-x86_64.tar.gz
 tar -zxvf cmake-3.30.5-linux-x86_64.tar.gz
 cd cmake-3.30.5-linux-x86_64 || exit 1
 sudo cp -r bin /usr/
