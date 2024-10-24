770c874e8b0f90fcab6ae0de73eb39b9c6a2251e
David Price <DPriceDev@gmail.com>
2024-10-25 00:11:57 +0100

    re add missing dependencies for grub and iso

--- .circleci/download-dependencies.sh
+++ .circleci/download-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install texinfo flex bison nasm graphviz doxygen ccache libmpfr-dev libmpc-dev doxygen unzip --no-install-recommends -y
+sudo apt-get install texinfo flex bison nasm graphviz doxygen ccache libmpfr-dev libmpc-dev doxygen unzip grub-common xorriso --no-install-recommends -y
